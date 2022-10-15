e024bb0b0c158f05ddae714f9913e838f8090549
AbyxDev <ken@abyx.dev>
2022-10-15 03:13:34 -0400

    add line utils

---
+++ src/best/view/utils.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QFrame
+
+class QHLine(QFrame):
+    def __init__(self):
+        super(QHLine, self).__init__()
+        self.setFrameShape(QFrame.Shape.HLine)
+        self.setFrameShadow(QFrame.Shadow.Sunken)
+
+class QVLine(QFrame):
+    def __init__(self):
+        super(QVLine, self).__init__()
+        self.setFrameShape(QFrame.Shape.VLine)
+        self.setFrameShadow(QFrame.Shadow.Sunken)
