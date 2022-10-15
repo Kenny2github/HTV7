c9f111a379c50910318a668eed9928c02f623061
Baraa710 <alchalabi.baraa@gmail.com>
2022-10-15 14:10:33 -0400

    Added comments

--- src/best/model/fullmodel.py
+++ src/best/model/fullmodel.py
@@ -7,11 +7,11 @@ from species import Species
 @dataclass
 class Ecosystem:
     allSpecies: list[Species]
-
+    #To add a species, we must enter its name, independent growth rate, dependent growth rate, and current population
     def addSpecies(self, name, indepGR = 0.0, depGR = dict(), population = 0.0):
         newSpecies = Species(name, indepGR, depGR, population)
         self.allSpecies.append(newSpecies)
-
+    
     def fullModel(self):
         r = np.matrix([
             [animal.indepGrowthRate] for animal in self.allSpecies
@@ -21,7 +21,7 @@ class Ecosystem:
             [animal.depGrowthRate[key] for key in animal.depGrowthRate]
             for animal in self.allSpecies
         ])
-
+        #model defines the ODE system
         def model(self, X: list[Species], t):
             dXdt = [animal.totalRate() for animal in X]
             return dXdt
@@ -36,7 +36,9 @@ class Ecosystem:
             # span for next time step
             tspan = [t[i-1],t[i]]
             # solve for next step
+            #z will be of type array[len[tspan], len[seld.allSpecies]]. In other words, the entry ij will be the population of j at time t
             z = odeint(model, self.allSpecies, tspan)
             # next initial condition
+            #z[0] is equal to
             for i, newP in enumerate(z[1]):
                 self.allSpecies[i].population = newP
