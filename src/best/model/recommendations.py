9a8981b80000d2cac35d3496b3bf9fdae2c1991c
Baraa710 <baraalchalabi@gmail.com>
2022-10-16 00:47:03 -0400

    recommendation sentences

--- src/best/model/recommendations.py
+++ src/best/model/recommendations.py
@@ -1,19 +1,14 @@
-import json
-from dataclasses import dataclass, field
-from typing import TypedDict
-import numpy as np
-from scipy.integrate import odeint
 from .species import Species
-from fullmodel import Ecosystem
+from .fullmodel import Ecosystem
 
 def giveRecs(S: Species, threshold:int, Eco: Ecosystem):
     preds = []
     preys = []
     if(S.population<threshold):
-        for s in S.depGrowthRate:
-            if (S.depGrowthRate[s] < 0):
+        for s, rate in S.depGrowthRate.items():
+            if (rate < 0):
                 preds = preds + s
-            if(S.depGrowthRate[s] >0):
+            if(rate >0):
                 preys= preys+s
     Rec = "Import/ breed/ feed the preys:" + [x for x in preys] + "and/or remove predsators:" + [y for y in preds]
     return Rec
