3c421537ed88c6063d23d983ff0b38d87ce08fbd
AbyxDev <ken@abyx.dev>
2022-10-16 03:42:26 -0400

    optimize tweak and make it an interruptible generator

--- src/best/model/tweak.py
+++ src/best/model/tweak.py
@@ -1,27 +1,52 @@
-from errno import ECHILD
+from typing import Generator, Generic, TypeVar, Union
 from .fullmodel import Ecosystem
-import time
+from .species import Species
 
-def findBetterInitP(ecosys: Ecosystem, ecoinit: Ecosystem) -> list[float]:
-    recChanges: list[float] = [0.0 for _ in ecosys.allSpecies]
+YT = TypeVar('YT')
+ST = TypeVar('ST')
+RT = TypeVar('RT')
+
+class ValueStoringGenerator(Generic[YT, ST, RT]):
+    gen: Generator[YT, ST, RT]
+    value: RT
+
+    def __init__(self, gen: Generator[YT, ST, RT]):
+        self.gen = gen
+
+    def __iter__(self) -> Generator[YT, ST, RT]:
+        self.value = yield from self.gen
+        return self.value
+
+    def send(self, value: ST) -> Union[YT, RT]:
+        try:
+            return self.gen.send(value)
+        except StopIteration as exc:
+            self.value = exc.value
+            return self.value
+
+def _findBetterInitP(ecosys: Ecosystem, ecoinit: Ecosystem
+                    ) -> Generator[int, bool, dict[Species, float]]:
+    recChanges = {species: 0.0 for species in ecosys.allSpecies}
     ecosim = ecoinit.clone()
-    extIndex = ecosys.extinction()
-    while extIndex:
-        for species in extIndex:
-            for i, (key, value) in enumerate(ecoinit.allSpecies[species].depGrowthRate.items()):
-                # if value > 0:
-                #     ecosys.allSpecies[i].population += 0.05
-                #     recChanges[i] += 0.05
-                # if value < 0:
-                #     ecosys.allSpecies[i].population -= 0.05
-                #     recChanges[i] -= 0.05
+    extinctions = ecosys.extinctions()
+    yield len(extinctions)
+    while extinctions:
+        for species in extinctions:
+            for key, value in species.depGrowthRate.items():
                 if value != 0:
-                    recChanges[i] += 1.0 / value
+                    recChanges[key] += 1.0 / value
             recChanges[species] += 1
-        for i in range(len(ecosys.allSpecies)):
-            ecosim.allSpecies[i].population = ecoinit.allSpecies[i].population + recChanges[i]
+        for simspecies, initspecies in zip(ecosim.allSpecies, ecoinit.allSpecies):
+            simspecies.population = initspecies.population + recChanges[initspecies]
         ecosim.fullModel()
-        extIndex = ecosim.extinction()
-        ecosim = ecoinit.clone()
+        extinctions = ecosim.extinctions()
+        for simspecies, initspecies in zip(ecosim.allSpecies, ecoinit.allSpecies):
+            simspecies.population = initspecies.population
+        if (yield len(extinctions)):
+            return recChanges
 
     return recChanges
+
+def findBetterInitP(ecosys: Ecosystem, ecoinit: Ecosystem
+                    ) -> ValueStoringGenerator[int, bool, dict[Species, float]]:
+    return ValueStoringGenerator(_findBetterInitP(ecosys, ecoinit))
